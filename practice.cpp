7ea082d6330e2a3392e93e5aa2db1e340412b928
rlatjdrbsqkq <rlatjdrbsqkq@naver.com>
2021-03-12 19:16:19 +0900

    2020.03.11 Add C++ Examples

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num1, num2; // 변수 num1, num2 선언
+	int num3 = 30, num4 = 40;
+	
+	printf("%d %d\n", num3, num4);
+	
+	num1 = 10;
+	num2 = 20;
+	
+	printf("%d %d", num1, num2); 
+}
